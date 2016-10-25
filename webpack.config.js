41fbaf4aeb38b776e24450f0a2334c32c494c787
marcandy <marcandy.nj@gmail.com>
2016-10-25 08:22:29 -0500

    working

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,25 @@ module.exports = {
 				test: /\.js/
 				, exclude: /node_modules/
 				, loader: 'babel'
-			}
-			, {
+			},
+				{ test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+					loader: "file"
+				},
+
+			 {
 				test: /\.css/
 				, exclude: /node_modules/
 				, loader: 'style-loader!css-loader'
-			}
+			},
+
+			{
+			 test: /.*\.(gif|png|jpe?g|svg)$/i,
+			 loaders: [
+				 'file?hash=sha512&digest=hex&name=[hash].[ext]',
+				 'image-webpack'
+			 ]
+		 }
+
 		]
 	}
 	, resolve: {
